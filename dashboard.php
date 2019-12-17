84eeb52e19efa240c45f829a270dcdccad95828d
unknown <ntboland@gmail.com>
2019-12-16 23:01:53 -0500

    added status filter

--- dashboard.php
+++ dashboard.php
@@ -70,27 +70,48 @@ ini_set('display_errors', 0);
 		<div class="col-md-3">
 			<a href="new.php" class="btn btn-theme btn-block">Add Job</a>
 		</div>
-		<div class="col-md-9"></div>
+		<div class="col-md-9" align="right">
+			<h5 class="small-header" style="font-weight: 600;"><a href="#" onclick="toggle_visibility('foo');">Filter <span class="fas fa-caret-down"></span></a></h5>
+		</div>
+	</div>
+	<div id="foo" class="row" style="padding-top: 20px;"> 
+		<div class="col-12">
+			<div class="floats" style="margin-bottom: 0px; padding: 10px 0px 10px 0px" align="center">
+				<button class="active btn btn-show" data-filter="box">Show All</button>
+	            <button class="btn btn-applied" data-filter="applied">Applied</button>
+	            <button class="btn btn-under-review" data-filter="under-review">Under Review</button>
+	            <button class="btn btn-interviewing" data-filter="interviewing">Interviewing</button>
+	            <button class="btn btn-offer-received" data-filter="offer-received">Offer Received</button>
+	            <button class="btn btn-rejected" data-filter="rejected">Rejected</button>
+			</div>
+		</div>
 	</div>
 	<hr>
 	<? $myVar = $list['id'] ?>
 	<?php foreach ($listing as $list): ?>
-	<div class="floats">
-		<div class="row" style="display: table; width:100%;">
-			<div class="statusColor">
-				<div class="status-tag"><span class="invisible"><?=$list['status']?></span></div>
-			</div>
-			<div class="col-10" style="display: table-cell; vertical-align: middle;">
-				<h2 class="job-title"><?=$list['title']?></h2>
-				<h5 class="small-header"><span style="font-weight: 600"><?=$list['company']?></span>&nbsp;&nbsp;&nbsp;<span class="fas fa-map-marker-alt"></span> <?=$list['location']?>&nbsp;&nbsp;&nbsp;<span title="Date Applied On"><span class="far fa-calendar-alt"></span> <?=$list['date_applied']?></span></h5>
-				<h5 class="small-header" style="font-style: italic;">Status: <span style="font-weight: 600; font-style: normal;"><?=ucwords($list['status'])?></span></h5>
-			</div>
-			<div class="col-2" style="display: table-cell; vertical-align: middle;">
-				<?php $myVar = $list['id'];?>
-				<form action="modify.php" method="get">
-					<input type="hidden" name="listid" value="<?php echo $myVar; ?>">
-					<button class="btn btn-theme btn-block">Modify</button>
-				</form>
+	<?php
+		//Allows two word statuses to be used as classes. Adds '-' between each word
+		$status = $list['status'];
+		$trimmedStatus = str_replace(' ', '-', trim($status));
+	?>
+	<div id="parent" >
+		<div class="floats box <?=$trimmedStatus?>">
+			<div class="row" style="display: table; width:100%;">
+				<div class="statusColor">
+					<div class="status-tag"><span class="invisible"><?=$list['status']?></span></div>
+				</div>
+				<div class="col-10" style="display: table-cell; vertical-align: middle;">
+					<h2 class="job-title"><?=$list['title']?></h2>
+					<h5 class="small-header"><span style="font-weight: 600"><?=$list['company']?></span>&nbsp;&nbsp;&nbsp;<span class="fas fa-map-marker-alt"></span> <?=$list['location']?>&nbsp;&nbsp;&nbsp;<span title="Date Applied On"><span class="far fa-calendar-alt"></span> <?=$list['date_applied']?></span></h5>
+					<h5 class="small-header" style="font-style: italic;">Status: <span style="font-weight: 600; font-style: normal;"><?=ucwords($list['status'])?></span></h5>
+				</div>
+				<div class="col-2" style="display: table-cell; vertical-align: middle;">
+					<?php $myVar = $list['id'];?>
+					<form action="modify.php" method="get">
+						<input type="hidden" name="listid" value="<?php echo $myVar; ?>">
+						<button class="btn btn-theme btn-block">Modify</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -103,41 +124,65 @@ ini_set('display_errors', 0);
 </div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
 <script>
 	$(function() {
-    //var text = $('.image-container>.status-tag').text().toLowerCase(), color;
-    $('.statusColor>.status-tag').each(function(){
-    	console.log($(this).text());
-      var text = $(this).text().toLowerCase();
-    	switch (text) {
-     	case 'applied':
-        color = '#BDC3C7';
-        break;
-     	case 'under review':
-        color = '#F1C40F';
-        break;
-        case 'interviewing':
-        color = '#3498DB';
-        break;
-        case 'offer received':
-        color = '#2ECC71';
-        break;
-        case 'rejected':
-        color = '#E74C3C';
-        break;
-     	default:
-        color = '#ECF0F1';
-    	}
-    	$(this).css('background', color);
-    });
-});
-	</script>
+	    //var text = $('.image-container>.status-tag').text().toLowerCase(), color;
+	    $('.statusColor>.status-tag').each(function(){
+	    	console.log($(this).text());
+	      var text = $(this).text().toLowerCase();
+	    	switch (text) {
+	     	case 'applied':
+	        color = '#BDC3C7';
+	        break;
+	     	case 'under review':
+	        color = '#F1C40F';
+	        break;
+	        case 'interviewing':
+	        color = '#3498DB';
+	        break;
+	        case 'offer received':
+	        color = '#2ECC71';
+	        break;
+	        case 'rejected':
+	        color = '#E74C3C';
+	        break;
+	     	default:
+	        color = '#ECF0F1';
+	    	}
+	    	$(this).css('background', color);
+	    });
+	});
 
+	      $(function(){
+			    var $boxs = $("#parent > .box");
+			    var $btns = $(".btn").on("click", function() {
 
+			      var active = 
+			        $btns.removeClass("active")
+			          .filter(this)
+			          .addClass("active")
+			          .data("filter");
+
+			      $boxs
+			        .hide()
+			        .filter( "." + active )
+			        .fadeIn(450);
+			    });
+			});
+
+	 function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'none')
+          e.style.display = 'block';
+       else
+          e.style.display = 'none';
+    }
+</script>
 
 </body>
 </html>
