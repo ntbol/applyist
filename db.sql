367b9cdf98a1c33a1cbd2d0d2f3849027c0c2c9c
unknown <ntboland@gmail.com>
2019-12-20 12:11:52 -0500

    updated database

--- db.sql
+++ db.sql
@@ -1,20 +1,30 @@
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-	`password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-	`email` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`),
-	UNIQUE KEY `username` (`username`)
-
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8  COLLATE=utf8_unicode_ci AUTO_INCREMENT;
-
+DROP TABLE IF EXISTS `listings`;
 CREATE TABLE IF NOT EXISTS `listings` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	'title' varchar(255) NOT NULL,
-	`company` varchar(255) NOT NULL,
-	`location` varchar(255) NOT NULL,
-	`link` varchar(1000) NULL,
-	`status` varchar(25) NOT NULL,
-	`user_id` int(10) NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `company` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `link` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `userid` int(10) NOT NULL,
+  `date_applied` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- --------------------------------------------------------
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `profile_img` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'gray.jpg',
+  `privilege` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+COMMIT;
